b2b2d680e1868f0cb976c7cdd269d220c6ea508b
Pavi1924 <pavithrav242@gmail.com>
2026-01-01 19:18:00 +0530

    Day 1: Java Basic Programs

---
+++ Area.java
@@ -0,0 +1,7 @@
+class Area {
+    public static void main(String[] args){
+        double radius = 10.1;
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of circle with radius " + radius + " is: " + area);
+    }
+}
---
+++ Me.java
@@ -0,0 +1,8 @@
+class Me{
+    public static void main(String [] args){
+        String name = "pavi";
+        int age = 20;
+        String clg = "mec";
+        System.out.println("Name: " + name + " Age: " + age + " College: " + clg);
+    }
+}
